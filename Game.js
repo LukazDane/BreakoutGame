d7012c2067096dc43a109ccd2beb02f11399a3fe
Luke <deandrevidal@aol.com>
2021-11-03 00:08:17 -0700

    [8/12] 75% done, mousemove not working.

--- Game.js
+++ Game.js
@@ -1,25 +1,26 @@
+/* eslint-disable import/extensions */
 /* eslint-disable no-alert */
-import Bricks from './Bricks';
-import Ball from './Ball';
-import Paddle from './Paddle';
-import GameLabel from './GameLabel';
+import Bricks from './Bricks.js';
+import Ball from './Ball.js';
+import Paddle from './Paddle.js';
+import GameLabel from './GameLabel.js';
 
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-const ballRadius = 10;
+// const ballRadius = 10;
 const paddleHeight = 10;
 const paddleWidth = 75;
 const brickRowCount = 9;
 const brickColumnCount = 4;
 const brickWidth = 75 / 2;
 const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
+// const brickPadding = 10;
+// const brickOffsetTop = 30;
+// const brickOffsetLeft = 30;
 const paddleXStart = (canvas.width - paddleWidth) / 2;
 const paddleYStart = canvas.height - paddleHeight;
-const PI2 = Math.PI * 2;
+// const PI2 = Math.PI * 2;
 const baseColour = '#0095DD';
 const gameOverMessage = 'Game Over';
 
@@ -30,16 +31,27 @@ class Game {
     this.paddle = new Paddle(paddleXStart, paddleYStart, paddleWidth, paddleHeight);
 
     this.scoreLabel = new GameLabel('Score: ', 8, 20);
-    this.livesLabel = new GameLabel('Lives: ', canvas.width - 65, 20, 'blue', 3);
+    this.livesLabel = new GameLabel('Lives: ', canvas.width - 65, 20, baseColour, 3);
 
     this.rightPressed = false;
     this.leftPressed = false;
 
     this.setup();
+    this.draw();
   }
 
   setup() {
     this.resetBAP();
+    // additional broken stuff
+    document.addEventListener('keydown', (e) => {
+      this.keyDownHandler(e);
+    }, false);
+    document.addEventListener('keyup', (e) => {
+      this.keyUpHandler(e);
+    }, false);
+    document.addEventListener('mousemove', (e) => {
+      this.mouseMoveHandler(e);
+    }, false);
   }
 
   resetBAP() {
@@ -72,6 +84,78 @@ class Game {
       }
     }
   }
-}
 
+  movePaddle() {
+    if (this.rightPressed && this.paddle.x < canvas.width - this.paddle.width) {
+      this.paddle.moveBy(7, 0);
+    } else if (this.leftPressed && this.paddle.x > 0) {
+      this.paddle.moveBy(-7, 0);
+    }
+  }
+
+  collisionWCAP() {
+    if (this.ball.x + this.ball.dx > canvas.width - this.ball.radius
+      || this.ball.x + this.ball.dx < this.ball.radius) {
+      this.ball.dx = -this.ball.dx;
+    }
+
+    if (this.ball.y + this.ball.dy < this.ball.radius) {
+      this.ball.dy = -this.ball.dy;
+    } else if (this.ball.y + this.ball.dy > canvas.height - this.ball.radius) {
+      if (this.ball.x > this.paddle.x && this.ball.x < this.paddle.x + this.paddle.width) {
+        this.ball.dy = -this.ball.dy;
+      } else {
+        this.livesLabel.value -= 1;
+        this.resetBAP();
+      } if (this.livesLabel.value < 1) {
+        alert(gameOverMessage);
+        this.ball.x = 200;
+        this.ball.y = 200;
+        document.location.reload();
+      } else {
+        // this.resetBAP();
+      }
+    }
+  }
+
+  keyDownHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+      this.rightPressed = true;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+      this.leftPressed = true;
+    }
+  }
+
+  keyUpHandler(e) {
+    if (e.key === 'Right' || e.key === 'ArrowRight') {
+      this.rightPressed = false;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+      this.leftPressed = false;
+    }
+  }
+
+  mouseMoveHandler(e) {
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (this.relativeX > 0 && relativeX < canvas.width) {
+      this.paddle.moveTo(relativeX - this.paddle.width / 2, paddleYStart);
+    }
+  }
+
+  draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.bricks.render(ctx);
+    this.ball.render(ctx);
+    this.paddle.render(ctx);
+    this.scoreLabel.render(ctx);
+    this.livesLabel.render(ctx);
+    this.collisionDetection();
+    this.ball.move();
+    this.collisionWCAP();
+    this.movePaddle();
+
+    requestAnimationFrame(() => {
+      this.draw(); // ****** broken
+    });
+  }
+}
 export default Game;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Brick from './Brick.js';
 import Bricks from './Bricks.js';
 import Paddle from './Paddle.js';
 import GameLabel from './GameLabel.js';
-// import Game from './Game.js';
+import Game from './Game.js';
 // import Lives from './Lives.js';
 // import Paddle from './Paddle.js';
 // import Score from './Score.js';
@@ -14,130 +14,21 @@ import GameLabel from './GameLabel.js';
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-const ballRadius = 10;
-const paddleHeight = 10;
-const paddleWidth = 75;
-const brickRowCount = 9;
-const brickColumnCount = 4;
-const brickWidth = 75 / 2;
-const brickHeight = 20;
-const brickPadding = 10;
-const brickOffsetTop = 30;
-const brickOffsetLeft = 30;
-const paddleXStart = (canvas.width - paddleWidth) / 2;
-const paddleYStart = canvas.height - paddleHeight;
-const PI2 = Math.PI * 2;
-const baseColour = '#0095DD';
-const gameOverMessage = 'Game Over';
+// const ballRadius = 10;
+// const paddleHeight = 10;
+// const paddleWidth = 75;
+// const brickRowCount = 9;
+// const brickColumnCount = 4;
+// const brickWidth = 75 / 2;
+// const brickHeight = 20;
+// const brickPadding = 10;
+// const brickOffsetTop = 30;
+// const brickOffsetLeft = 30;
+// const paddleXStart = (canvas.width - paddleWidth) / 2;
+// const paddleYStart = canvas.height - paddleHeight;
+// const PI2 = Math.PI * 2;
+// const baseColour = '#0095DD';
+// const gameOverMessage = 'Game Over';
+const game = new Game();
 
-
-resetBAP();
-
-let rightPressed = false;
-let leftPressed = false;
-
-function collisionDetection() {
-  for (let c = 0; c < bricks.cols; c += 1) {
-    for (let r = 0; r < bricks.rows; r += 1) {
-      const brick = bricks.bricks[c][r];
-      if (brick.status === 1) {
-        // eslint-disable-next-line max-len
-        if (ball.x > brick.x && ball.x < brick.x + brickWidth && ball.y > brick.y && ball.y < brick.y + brickHeight) {
-          ball.dy = -ball.dy;
-          brick.status = 0;
-
-          scoreLabel.value += 1;
-
-          if (scoreLabel.value === bricks.cols * bricks.rows
-          ) {
-            alert('YOU WIN, CONGRATS!');
-            document.location.reload();
-          }
-        }
-      }
-    }
-  }
-}
-
-function resetBAP() {
-  ball.x = canvas.width / 2;
-  ball.y = canvas.height - 30;
-  ball.dx = 2;
-  ball.dy = -2;
-  paddle.x = paddleXStart;
-}
-
-function collisionWCAP() {
-  if (ball.x + ball.dx > canvas.width - ballRadius || ball.x + ball.dx < ballRadius) {
-    ball.dx = -ball.dx;
-  }
-
-  if (ball.y + ball.dy < ballRadius) {
-    ball.dy = -ball.dy;
-  } else if (ball.y + ball.dy > canvas.height - ballRadius) {
-    if (ball.x > paddle.x && ball.x < paddle.x + paddle.width) {
-      ball.dy = -ball.dy;
-    } else {
-      livesLabel.value -= 1;
-      resetBAP();
-    } if (livesLabel.value < 1) {
-      alert(gameOverMessage);
-      ball.x = 200;
-      ball.y = 200;
-      document.location.reload();
-    } else {
-      // resetBAP();
-    }
-  }
-}
-
-function movePaddle() {
-  if (rightPressed && paddle.x < canvas.width - paddle.width) {
-    paddle.moveBy(7, 0);
-  } else if (leftPressed && paddle.x > 0) {
-    paddle.moveBy(-7, 0);
-  }
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  bricks.render(ctx);
-  ball.render(ctx);
-  paddle.render(ctx);
-  scoreLabel.render(ctx);
-  livesLabel.render(ctx);
-  collisionDetection();
-  ball.move();
-  collisionWCAP();
-  movePaddle();
-
-  requestAnimationFrame(draw);
-}
-
-function keyDownHandler(e) {
-  if (e.key === 'Right' || e.key === 'ArrowRight') {
-    rightPressed = true;
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    leftPressed = true;
-  }
-}
-
-function mouseMoveHandler(e) {
-  const relativeX = e.clientX - canvas.offsetLeft;
-  if (relativeX > 0 && relativeX < canvas.width) {
-    paddle.moveTo(relativeX - paddle.width / 2, paddleYStart);
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.key === 'Right' || e.key === 'ArrowRight') {
-    rightPressed = false;
-  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
-    leftPressed = false;
-  }
-}
-
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
-document.addEventListener('mousemove', mouseMoveHandler, false);
-draw();
+game.draw();
